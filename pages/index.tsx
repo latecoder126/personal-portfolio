6fe68f38f7a9f8ece7b6bd7827ce570429e1bc79
= <latecoder126@gmail.com>
2023-04-15 18:28:56 +0530

    j

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export const getStaticProps = async () => {
     const props = await resolveNotionPage(domain)
     return { props, revalidate: 10 }
   } catch (err) {
-    console.error('page error', domain, err)
+    // console.error('page error', domain, err)
     // we don't want to publish the error version of this page, so
     // let next.js know explicitly that incremental SSG failed
     throw err
