de2e35b84c4c4fc45b3c3e3c20cc76e2e1570600
= <latecoder126@gmail.com>
2023-04-15 20:13:16 +0530

    fds

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,16 @@ import { domain } from '@/lib/config'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
 
 export const getStaticProps = async () => {
-
-  const props = await resolveNotionPage(domain)
+  
+  try {
+    const props = await resolveNotionPage(domain)
     return { props, revalidate: 10 }
-  // try {
-  //   const props = await resolveNotionPage(domain)
-  //   return { props, revalidate: 10 }
-  // } catch (err) {
-  //   console.error('page error', domain, err)
-  //   // we don't want to publish the error version of this page, so
-  //   // let next.js know explicitly that incremental SSG failed
-  //   throw err
-  // }
+  } catch (err) {
+    console.error('page error', domain, err)
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err
+  }
 }
 
 export default function NotionDomainPage(props) {
