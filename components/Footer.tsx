aaa73716dec097025cdb7fa1ce6f0113f0400daf
latecoder126 <129985414+latecoder126@users.noreply.github.com>
2024-06-01 13:26:41 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-     <div className={styles.copyright}> Copyright &copy; 2023 || 🧑‍💻 & Crafted with 💚 by 
+     <div className={styles.copyright}> Copyright &copy; 2024 || 💚  
               <a href="https://www.linkedin.com/in/sandeepningwal/"
               target="_blank" rel="noreferrer" > {config.author}.</a></div>
 
